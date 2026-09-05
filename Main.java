51b1d3fa80de13b75241a7b5f1fb358d7a27ce59
Paulo <paulo.trimpler@escola.pr.gov.br>
2026-09-05 13:18:31 -0300

    Adicionando licença e modificando arquivos

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
     }
     public static int subtracao(int A, int B) {
         return A - B;
