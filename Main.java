dba9c07398fcd1053913e52f29314b3e9eb98e7c
Paulo <paulo.trimpler@escola.pr.gov.br>
2026-09-05 13:45:22 -0300

    Modificação do Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
     }
     public static int subtracao(int A, int B) {
         return A - B;
-    }
+    }  
 }
