655b3377d78d284d820daaed9de6722a84007194
Paulo <paulo.trimpler@escola.pr.gov.br>
2026-09-04 21:32:13 -0300

    Criação do arquivo Java, configuração do .gitignore e adição do arquivo informações.txt

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    public static int subtracao(int A, int B) {
+        return A - B;
+    }
+}
