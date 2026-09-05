9feba1a7e77a3a9d9ada7b990714af3db246b814
Paulo <paulo.trimpler@escola.pr.gov.br>
2026-09-05 14:03:29 -0300

    Adição da função print()

--- Main.java
+++ Main.java
@@ -8,4 +8,15 @@ public class Main {
     public static int subtracao(int A, int B) {
         return A - B;
     }  
+    public static void print(int tipo, String texto) {
+        if (tipo == 1) {
+            System.out.println(texto);
+        } else if (tipo == 2) {
+            print(1, "");
+            print(1, "========================");
+            print(1, "");
+        } else if (tipo == 3) {
+            System.err.println(texto);
+        }
+    }
 }
