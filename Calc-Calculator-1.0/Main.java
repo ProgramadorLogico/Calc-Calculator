735dc14acb3128db3a6aad13ca430de9c1fab892
Paulo <paulo.trimpler@escola.pr.gov.br>
2026-09-05 21:00:27 -0300

    Alteração de arquivos importantes

--- Calc-Calculator-1.0/Main.java
+++ Calc-Calculator-1.0/Main.java
@@ -3,37 +3,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
+    // Listas
+    public static final ArrayList<String> comandosExistentes = new ArrayList<> (Arrays.asList("1 - Fazer uma operação", "2 - Informações do projeto", "3 - Fechar programa"));
     public static void main(String[] args) {
         // Variáveis
         Scanner scanner = new Scanner(System.in);
         String versaoDoProjeto = "1.0";
         String nomeDoProjeto = "Calc Calculator";
         String criadorDoProjeto = "Programador Lógico";
-        // Listas
-        ArrayList<String> comandosExistentes = new ArrayList<> (Arrays.asList("1 - Fazer uma operação", "2 - Informações do projeto", "3 - Fechar programa"));
         // Apresentação ao usuário
-        print(1, "================================================");
+	print(1, "================================================");
         print(1, "");
         print(1, "Bem-vindo ao Calc Calculator!");
         print(1, "Você está utilizando a versão" + versaoDoProjeto);
         print(1, "Caso queira a versão mais recente, acesse: https://github.com/ProgramadorLogico/Calc-Calculator e baixe a versão mais recente");
-        print(2, "");
         // Lista todos os comandos
-        for (int i = 0; i < comandosExistentes.size(); i++) {
-            System.out.println(comandosExistentes.get(i));
-        }
+	listarComandos();
+	// Loop principal
         while (true) {
             // Pegar entrada do usuário
             String comandoSelecionado = scanner.nextLine();
-            // Fecha o enter
-            scanner.nextLine();
             // Verifica qual comando foi selecionado
             switch (comandoSelecionado) {
                 case "1":
-                    // Fazer depois
-                    print(2, "");
-                    print(1, "Em breve!");
-                    break;
+		    double primeiroNumeroValor;
+		    double segundoNumeroValor;
+		    String operacao;
+                    double primeiroNumero = new scanner.nextDouble();
+		    if (primeiroNumero.hasNextInt || primeiroNumero.hasNextDouble) {
+			primeiroNumero = primeiroNumeroValor;
+		    } else {
+			print(2, "");
+			print(3, "O valor digitado não é válido!");
+		    }
+		    listarComandos(); break;
                 case "2":
                     // Exibi as informações do projeto
                     print(2, "");
@@ -42,6 +45,7 @@ public class Main {
                     System.out.println("Nome do projeto: " + nomeDoProjeto);
                     System.out.println("Versão do projeto: " + versaoDoProjeto);
                     System.out.println("Criador do projeto: " + criadorDoProjeto);
+		    listarComandos();
                     break;
                 case "3":
                     // Fecha o programa
@@ -51,13 +55,14 @@ public class Main {
                     // Retorna um aviso de comando inválido
                     print(2, "");
                     print(3, "O comando digitado não existe");
+		    listarComandos();
                     break;
             }
         }
     }
     public static int subtracao(int A, int B) {
         return A - B;
-    }  
+    }
     public static void print(int tipo, String texto) {
         if (tipo == 1) {
             System.out.println(texto);
@@ -70,5 +75,11 @@ public class Main {
         } else {
             print(3, "A variável tipo recebeu um valor inválido");
         }
+    }
+    public static void listarComandos() {
+	print(2, "");
+	for (int i = 0; i < comandosExistentes.size(); i++) {
+	    System.out.println(comandosExistentes.get(i));
+	}
     }
 }
